f2b7c88545eff5495fcd3002545db8fa1daef0ec
colbertamir <colbertamir1516@gmail.com>
2023-11-13 08:43:27 -0600

    Pushin 0 need to install flake8

---
+++ web_flask/0-hello_route.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""Starting up Flask & returning Hello HBNB!"""
+
+from flask import Flask
+
+
+app = Flask(__name__)
+
+@app.route('/', strict_slashes=False)
+def hello_there():
+    return 'Hello HBNB!'
+
+if __name__ == '__main__':
+    # Run the Flask app on 0.0.0.0 (accessible from any IP) and port 5000
+    app.run(host='0.0.0.0', port=5000)
