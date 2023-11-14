bd722e3a0044aa8c1b42b88b1e19181fc89e927f
colbertamir <colbertamir1516@gmail.com>
2023-11-13 22:48:23 -0600

    Python is cool! & Changes to 0

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -12,5 +12,4 @@ def hello_there():
     return 'Hello HBNB!'
 
 if __name__ == '__main__':
-    # Run the Flask app on 0.0.0.0 (accessible from any IP) and port 5000
     app.run(host='0.0.0.0', port=5000)
