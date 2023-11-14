4157af5fb6adf59430ed1fc01311cc70d6b4d9ef
colbertamir <colbertamir1516@gmail.com>
2023-11-14 15:50:31 -0600

    Corrections to base_model.py

--- models/base_model.py
+++ models/base_model.py
@@ -1,24 +1,24 @@
-#!/usr/bin/python3
-"""This module defines a base class for all models in our hbnb clone"""
 import uuid
 from datetime import datetime
-
+from models import storage
 
 class BaseModel:
-    """A base class for all hbnb models"""
+    """A base class for hbnb models"""
+
     def __init__(self, *args, **kwargs):
-        """Instatntiates a new model"""
+        """Instantiates a new model"""
         if not kwargs:
-            from models import storage
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
             storage.new(self)
         else:
-            kwargs['updated_at'] = datetime.strptime(kwargs['updated_at'],
-                                                     '%Y-%m-%dT%H:%M:%S.%f')
-            kwargs['created_at'] = datetime.strptime(kwargs['created_at'],
-                                                     '%Y-%m-%dT%H:%M:%S.%f')
+            kwargs['updated_at'] = datetime.strptime(
+                kwargs['updated_at'], '%Y-%m-%dT%H:%M:%S.%f'
+            )
+            kwargs['created_at'] = datetime.strptime(
+                kwargs['created_at'], '%Y-%m-%dT%H:%M:%S.%f'
+            )
             del kwargs['__class__']
             self.__dict__.update(kwargs)
 
@@ -28,17 +28,14 @@ class BaseModel:
         return '[{}] ({}) {}'.format(cls, self.id, self.__dict__)
 
     def save(self):
-        """Updates updated_at with current time when instance is changed"""
-        from models import storage
+        """Updates updated_at with the current time when instance is changed"""
         self.updated_at = datetime.now()
         storage.save()
 
     def to_dict(self):
         """Convert instance into dict format"""
-        dictionary = {}
-        dictionary.update(self.__dict__)
-        dictionary.update({'__class__':
-                          (str(type(self)).split('.')[-1]).split('\'')[0]})
+        dictionary = self.__dict__.copy()
+        dictionary['__class__'] = self.__class__.__name__
         dictionary['created_at'] = self.created_at.isoformat()
         dictionary['updated_at'] = self.updated_at.isoformat()
         return dictionary
